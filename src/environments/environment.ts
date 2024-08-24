b5d60fd59a369b9ddf0b6da8029645e08b1da546
Jhon Livias <jhonlivias3@gmail.com>
2024-08-24 12:54:04 -0500

    update environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   BASE_URL: 'http://127.0.0.1:8000/api',
-  //BASE_URL: 'https://backend.zerogroups.uk/api',
+  //BASE_URL: 'https://sslaiss.zerogroups.uk/api',
 };
